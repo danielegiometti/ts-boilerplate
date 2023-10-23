a32c88b2b1a9f1a72823fb6741743a25ccd463d0
Daniele Giometti <daniele.giometti@thetrainline.com>
2023-10-23 17:35:39 +0100

    adding controller api

--- src/Boilerplate/app.ts
+++ src/Boilerplate/app.ts
@@ -1,20 +1,27 @@
-import { SQSEvent } from 'aws-lambda';
+import { APIGatewayEvent, ProxyResult, SQSEvent } from 'aws-lambda';
 import Symbols from '../Boilerplate.Models/Symbols';
 import IOC from './IOCContainer/IOC';
 import IBoilerplateManager from '../Boilerplate.Domain/Interfaces/IBoilerplateManager';
 import IBoilerPayload from '../Boilerplate.Models/Interfaces/IBoilerPayload';
 
-export const initialiseContainer = () => {
-    const ioc = new IOC();
-    return ioc.GetInstance();
-};
+const initialiseContainer = async () => await new IOC().GetInstance();
 
 export const handler = async (event: SQSEvent): Promise<void> => {
-    const containerInstance = initialiseContainer();
+    const containerInstance = await initialiseContainer();
     const mutationManager = containerInstance.get<IBoilerplateManager>(Symbols.BoilerplateManager);
 
     for (const record of event.Records) {
         const order = JSON.parse(record.body) as IBoilerPayload;
         await mutationManager.Manage(order);
     }
+};
+
+export const createBoiler = async (event: APIGatewayEvent): Promise<void | ProxyResult> => {
+
+    const containerInstance = await initialiseContainer();
+    const mutationManager = containerInstance.get<IBoilerplateManager>(Symbols.BoilerplateManager);
+
+    const payload: IBoilerPayload = JSON.parse(event.body!) as IBoilerPayload;
+    
+    return await mutationManager.Manage(payload);
 };
